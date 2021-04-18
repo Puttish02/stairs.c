3e1d4ce4804cc774bbf237a4cfecfb9eaae8a82a
Puttish02 <82376006+Puttish02@users.noreply.github.com>
2021-04-18 11:16:08 +0530

    Stairs.c

---
+++ Stairs.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ #include <conio.h>
+
+main()
+{
+int i,j,n;
+{
+printf("enter n :");
+scanf("%d",&n);
+}
+for (i=0; i<n; i++)
+{
+for(j=0; j<= i; j++)
+{
+
+if ((i+j)%2==0)
+{
+
+ printf("0");
+}
+else
+{
+printf("1");
+}
+}
+printf("\n");
+}
+getch ();
+}
+
+  
